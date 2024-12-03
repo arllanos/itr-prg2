1e1ee867ef5a311045e2605dc58aee9e0c3323c6
matias <matiasjoel.llanos@gmail.com>
2024-12-03 13:18:21 -0300

    ej4

--- classroom/2024-03/ej4.py
+++ classroom/2024-03/ej4.py
@@ -10,8 +10,8 @@ def isPrime(num):
 
 def ObtenerPrimosHasta(n):
     hasta = int(math.sqrt(n))
-
     factores = []
+
     for i in (range(2, hasta)):
         if isPrime(i):
             factores.append(i)
@@ -19,8 +19,9 @@ def ObtenerPrimosHasta(n):
 
 def PrimeFactors(num):
     primos = ObtenerPrimosHasta(num)
-    # if num > 1:
+    #primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307]
     factores = []
+
     for i in primos:
         while num % i == 0:
             factores.append(i)
@@ -28,9 +29,8 @@ def PrimeFactors(num):
     return factores
 
 fp = PrimeFactors(60)
-
 print(fp)
-
+# CHECK
 resultado = 1
 for n in fp:
     resultado = resultado * n
