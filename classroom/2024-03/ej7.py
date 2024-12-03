f5135c367e64a4114b48faad08f90b5021ab6cfb
Ariel Llanos <ariel.llanos@gmail.com>
2024-12-03 20:23:32 -0300

    ej7

---
+++ classroom/2024-03/ej7.py
@@ -0,0 +1,16 @@
+"""
+Implementar una función que calcule la codificación y de decodificación
+utilizando el método de codificación de longitud de carrera.
+Ayuda: La codificación de longitud de carrera (RLE) es una forma sencilla de compresión de
+datos, en la que las que los elementos de datos consecutivos se sustituyen por un solo
+valor de datos y un recuento.
+Ejemplos:
+Por ejemplo, podemos representar los 53 caracteres originales con sólo 13
+WWWWWWWWWWWWBWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+
+=>
+
+WWWWWWWWWWWW                                    12 W
+B                                               1  B
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW        40 W
+"""
