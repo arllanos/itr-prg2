7d63fff3d908c78aa0906790dd400770ea2743f2
Ariel Llanos <ariel.llanos@gmail.com>
2024-12-04 16:32:53 -0300

    ej8-recursive

---
+++ classroom/2024-03/ej8_recursive.py
@@ -0,0 +1,15 @@
+
+
+def collatz(num):
+    print(num)
+    # condicion de corte
+    if num == 1:
+        return num
+    
+    if num % 2 == 0:
+        collatz(num/2)
+    else:
+        collatz((3*num) + 1)
+
+r = collatz(22)
+print(r)
