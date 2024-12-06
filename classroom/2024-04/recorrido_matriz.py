d5ac8f599c846c0ab94febe1b3a46f41ad0f8bd3
matias <matiasjoel.llanos@gmail.com>
2024-12-05 22:14:21 -0300

    recorrido

---
+++ classroom/2024-04/recorrido_matriz.py
@@ -0,0 +1,15 @@
+
+matriz = [
+        [2,3,4,40],
+        [5,6,7,70],
+        [8,9,10,100]
+        ]
+
+
+# for pepe in matriz:
+for x in range(len(matriz)):
+    for y in range(len(matriz[0])):
+        print(f" {x}, {y}: {matriz[x][y]}")
+
+
+
